960deba0f574833b9658c3df76e73f37c00d7bd0
void-kun <hoangzrik@gmail.com>
2024-02-21 22:05:16 +0700

    update

--- .config/Code/User/History/-40efe4b6/z8rL.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-polybar-msg cmd quit
-
-# Launch statusbar_bot
-echo "---" | tee -a /tmp/statusbar_bot.log 
-polybar statusbar_bot -c ~/.config/polybar/config/statusbar.ini 2>&1 | tee -a /tmp/statusbar_bot.log & disown
-
-echo "Bars launched..."
