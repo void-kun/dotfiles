ae0e6de73cb3b341deccbc326e039ffe9a759c09
zrik <zrik@zrik>
2024-03-20 19:10:27 +0700

    update

--- .emacs.d/core/lolo-funcs.el
+++
@@ -1,7 +0,0 @@
-;;; lolo-funcs.el --- Zrik's Emacs setup.  -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
-
-        
-(provide 'lolo-funcs)
-;;; lolo-funcs.el ends here
--- .emacs.d/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-python script.py new -d $1 -t $2
